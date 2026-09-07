1ad140f3ecd4093c58aceb9a77518b1e238b9d8b
zmalik2030 <zmaliks2030@gmail.com>
2026-09-07 18:37:18 +0500

    aa

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=], initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    welcome
-</body>
-</html>
+<?php
+
+$hostname = 'uzgoah.stackhero-network.com';
+$port = '7406';
+$user = 'root';
+$password = 'zukBywBtRPTqfrwFTTqv2lPYG1cjKaPn';
+$database = 'root'; // While this example uses the "root" database, it is a good idea to create a separate database and user for your application via phpMyAdmin.
+
+$mysqli = mysqli_init();
+$mysqliConnected = $mysqli->real_connect($hostname, $user, $password, $database, $port, NULL, MYSQLI_CLIENT_SSL);
+if (!$mysqliConnected) {
+  die("Connection Error: " . $mysqli->connect_error);
+}
+
+echo 'Connection successful... ' . $mysqli->host_info . "\n";
+
+$mysqli->close();
+
+?>
